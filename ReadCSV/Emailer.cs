cc7a903a13d1050c1ddc44bc8454bacf4ac35373
Yustin1999 <justinsummers@outlook.com>
2025-07-31 19:41:47 +0100

    Read csv + send email
    Reads the CSV file and then sends email including the attachment.

---
+++ ReadCSV/Emailer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WMT.Emailer
+{
+    using System;
+    using System.Net;
+    using System.Net.Mail;
+
+public class Emailer
+{
+        public static void SendEmail(string message, string path)
+    {
+        try
+        {
+            string password = File.ReadAllText("C:\\Users\\Justi\\source\\repos\\Yustin1999\\ReadCSV\\ReadCSV\\password.txt").Trim();
+                var smtpClient = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential("justinsummers124@gmail.com", password),
+                EnableSsl = true,
+            };
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("justinsummers123@gmail.com"),
+                Subject = "WMT Morning Checks",
+                Body = message,
+                IsBodyHtml = true,
+                
+            };
+            mailMessage.Attachments.Add(new Attachment(path));
+            mailMessage.To.Add("justin@newparksolutions.com");
+
+            smtpClient.Send(mailMessage);
+            Console.WriteLine("Email sent successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error sending email: " + ex.Message);
+        }
+    }
+}
+}
--- ReadCSV/Program.cs
+++ ReadCSV/Program.cs
@@ -2,10 +2,12 @@
 using CsvHelper;
 using System.Data;
 using System.Globalization;
-
-using (var reader = new StreamReader("C:\\Users\\JustinSummers\\source\\repos\\ReadCSV\\ReadCSV\\Data\\TEST.csv"))
+using WMT.Emailer;
+string path = "C:\\Users\\Justi\\Downloads\\TEST.CSV";
+using (var reader = new StreamReader("C:\\Users\\Justi\\Downloads\\TEST.CSV"))
 using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
 {
+    string sendMessage = "";
     using (var dr = new CsvDataReader(csv)) {
         var dt = new DataTable();
         dt.Columns.Add("Area", typeof(string));
@@ -15,27 +17,51 @@ using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         dt.Load(dr);
 
         Dictionary<string, int[]> values = new Dictionary<string, int[]>();
-
+        List<string> messages = new List<string>();
+        
         for (int i = 0; i < dt.Rows.Count; i++) {
             int percent = Convert.ToInt32(dt.Rows[i]["Percent"]);
             string area = dt.Rows[i]["area"].ToString();
             int entered = Convert.ToInt32(dt.Rows[i]["All Entered"]);
             int exited = Convert.ToInt32(dt.Rows[i]["All Exited"]);
-
+            
             if (percent >= 8.00) {
                 values.Add(area, [percent, entered, exited]);
             }
         }
         foreach (var pair in values){
             if (pair.Value[1] > pair.Value[2]){
-                Console.WriteLine($"Area: {pair.Key}, Entry-Exit % difference: {pair.Value[0]}% , Total vehicles entered: {pair.Value[1]} , Total vehicles Exited: {pair.Value[2]}  - Please check the Exit cameras for this site");
-            }else {
-                Console.WriteLine($"Area: {pair.Key}, Entry-Exit % difference: {pair.Value[0]}% , Total vehicles entered: {pair.Value[1]} , Total vehicles Exited: {pair.Value[2]}  - Please check the Entry cameras for this site");
+                //messages.Add($"Area: {pair.Key}, Entry-Exit % difference: {pair.Value[0]}% , Total vehicles entered: {pair.Value[1]} , Total vehicles Exited: {pair.Value[2]}  - Please check the Exit cameras for this site");
+                messages.Add($@"
+                    <b>Area: {pair.Key}</b>
+                    <ul>
+                        <li>Entry-Exit % difference: {pair.Value[0]}%</li>
+                        <li>Total vehicles entered: {pair.Value[1]}</li>
+                        <li>Total vehicles exited: {pair.Value[2]}</li>
+                        <li>Please check the Entry cameras for this site</li>
+                    </ul>");
+            }
+            else {
+                //messages.Add($"Area: {pair.Key}, Entry-Exit % difference: {pair.Value[0]}% , Total vehicles entered: {pair.Value[1]} , Total vehicles Exited: {pair.Value[2]}  - Please check the Entry cameras for this site");
+                messages.Add($@"
+                    <b>Area: {pair.Key}</b>
+                    <ul>
+                        <li>Entry-Exit % difference: {pair.Value[0]}%</li>
+                        <li>Total vehicles entered: {pair.Value[1]}</li>
+                        <li>Total vehicles exited: {pair.Value[2]}</li>
+                        <li>Please check the Exit cameras for this site</li>
+                    </ul>");
             }
                
         }
-        
+        string emailBody = string.Join("<br/>", messages);
+        if (messages.Count == 0)
+        {
+            emailBody = "No issues found for today.";
+        }
+            Emailer.SendEmail(emailBody,path);
     }
+
     
 }
 
